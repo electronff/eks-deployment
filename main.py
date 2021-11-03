49d47c809fc162e2ba162c909aa4d4e933023a3f
electronff <aminumuritala@gmail.com>
2021-11-03 12:35:31 -0500

    u

--- main.py
+++ main.py
@@ -4,24 +4,38 @@ import os
 from flask import Flask
 from flask import request
 app = Flask(__name__)
-
+#print(os.environ)
+os.environ["RESPONSE"]='success'
 RESPONSE = os.environ["RESPONSE"]
+
+@app.route("/")
+def homepage():
+    msg = {'message': 'This is Home for me...'}
+    return json.dumps(msg)
+
+
+@app.route("/aminu")
+def myname():
+    msg = {'message': 'I am Aminu...'}
+    return json.dumps(msg)
+
+
 
 @app.route("/status")
 def root():
 	ret = {
-		'response': RESPONSE
+		'result': RESPONSE
 	}
 
 	return json.dumps(ret)
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True, host='0.0.0.0', port=8080)
 
 # # to be deleted
 
-# import web
+#import web
 
-# if __name__ == '__main__':
-#     web.app.run(debug=True, host='0.0.0.0', port=8080)
+#if __name__ == '__main__':
+#    web.app.run(debug=True, host='0.0.0.0', port=8080)
 	
